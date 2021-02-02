25dbe86e5b4cfc1e5f48592ba2906653f5bbaab8
Erick Bernardo <erickvictor@gmail.com>
2021-02-02 08:51:39 -0400

    Add config proxy

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 // Listen on a specific host via the HOST environment variable
-var host = process.env.HOST || '0.0.0.0';
+var host = process.env.HOST || "0.0.0.0";
 // Listen on a specific port via the PORT environment variable
 var port = process.env.PORT || 8080;
 
-var cors_proxy = require('cors-anywhere');
+var cors_proxy = require("cors-anywhere");
 cors_proxy
   .createServer({
     originWhitelist: [], // Allow all origins
-    requireHeader: ['origin', 'x-requested-with'],
-    removeHeaders: ['cookie', 'cookie2'],
+    requireHeader: ["origin", "x-requested-with"],
+    checkRateLimit: checkRateLimit,
+    removeHeaders: [
+      "cookie",
+      "cookie2",
+      "x-request-start",
+      "x-request-id",
+      "via",
+      "connect-time",
+      "total-route-time",
+    ],
+    redirectSameOrigin: true,
+    httpProxyOptions: {
+      // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
+      xfwd: false,
+    },
   })
   .listen(port, host, function () {
-    console.log('Running CORS Anywhere on ' + host + ':' + port);
+    console.log("Running CORS Anywhere on " + host + ":" + port);
   });
